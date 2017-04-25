5901422f3b956c84fdb21791f4bdbb0b8215dacb
Антон Степаненков <Lenseg1@gmail.com>
2017-04-25 23:20:21 +0300

    wifi form validation init

--- src/app/wifi-form/wifi-form.component.ts
+++ src/app/wifi-form/wifi-form.component.ts
@@ -10,7 +10,7 @@ import { ErrorMessage } from '../global/typeClasses';
 })
 export class WifiFormComponent {
   form : FormGroup;
-
+  typeCache : string;
 
   typeErrors : ErrorMessage[] = [];
   nameErrors : ErrorMessage[] = [];
@@ -20,6 +20,7 @@ export class WifiFormComponent {
 
   constructor(private fb:FormBuilder, private createCodeService:CreateCodeService){
     this.createForm();
+    this.typeCache = this.form.value.type;
     this.bindUpdateEvents();
   }
   createForm():void{
@@ -30,17 +31,23 @@ export class WifiFormComponent {
       pass: '',
       type:'WPA',
       hidden:false
+    },{
+      validator: this.isPasswordReqired('type','pass')
     });
     this.bindUpdateEvents();
   }
   bindUpdateEvents():void{
-    this.form.valueChanges.subscribe((value:string) => {
+    this.form.valueChanges.subscribe((value:any) => {
+      if(this.checkIsTypeChanged(value.type)){
+        this.setFormConfigByType(value.type)
+      }
       for(const controlName in errors){
         const control = this.form.get(controlName);
         this[controlName+'Errors'] = [];
         this[controlName+'Warns'] = [];
         if (control && control.dirty && !control.valid) {
           for (const key in control.errors) {
+            console.log('key')
             const errMessage = errors[controlName][key];
             if(errMessage.type === 'err'){
               this[controlName+'Errors'].push(errMessage)
@@ -53,18 +60,72 @@ export class WifiFormComponent {
       this.sendModel();
     });
   }
+  isPasswordReqired(typeControlName:string,passControlName:string){
+    return(group: FormGroup) => {
+      let typeControl = group.get(typeControlName);
+      let passControl = group.get(passControlName);
+      console.log(passControl, typeControl.value !== 'nopass', !passControl.untouched, passControl.value === '',passControl.value)
+      if(typeControl.value !== 'nopass' && !passControl.untouched && passControl.value === ''){
+        return passControl.setErrors({required: true})
+      } else {
+        return passControl.setErrors({})
+      }
+    }
+  }
+  checkIsTypeChanged(type:string){
+    if(type!==this.typeCache){
+      this.typeCache = type;
+      return true
+    }
+    return false;
+  }
+
+  setFormConfigByType(type:string){
+    let passControll = this.form.get('pass');
+    if(type=== 'nopass'){
+      passControll.disable();
+      passControll.setValue('',{
+        emitEvent:true
+      });
+    } else {
+      passControll.enable();
+    }
+  }
+
   sendModel():void{
-    var number = this.form.value.number.replace(/[^+[0-9]]*/g,'');
-    this.createCodeService.codeValueUpdate('SMSTO:' + number + ':' + this.form.value.message);
+    this.createCodeService.codeValueUpdate(this.constructWifiString());
+  }
+
+  constructWifiString():string{
+    var code = 'WIFI:' + 'T:' + this.form.value.type + ';';
+    code += 'S:' + this.encodeStringComponent(this.form.value.name) + ';';
+    if(this.form.value.type !== 'nopass'){
+      code += 'P:' + this.encodeStringComponent(this.form.value.pass) + ';';
+    }
+    if(this.form.value.hidden){
+      code += 'H:true;';
+    }
+    return code
+  }
+
+  encodeStringComponent(str:string):string{
+    var encodedStr = str.replace('"','\\"').replace('"','\\"').replace(',','\\,').replace(';','\\;').replace(':','\\:');
+    if(parseInt(encodedStr,16).toString(16) === encodedStr)
+      encodedStr = '"' + encodedStr + '"';
+    return encodedStr
   }
 }
 const errors = {
   name:{
     required : {
       type:'err',
-      message:'Network name is required.'
+      message:'Network name required.'
     }
   },
-  password : {
+  pass : {
+    required : {
+      type:'err',
+      message:'Password  required.'
+    }
   }
 }
