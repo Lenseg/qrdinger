d62db45f5d1aa659781b013a8645ddeb524a6ec8
Антон Степаненков <Lenseg1@gmail.com>
2017-09-18 17:49:27 +0300

    redirect qr backend mvp

--- qr-back/server.js
+++ qr-back/server.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
-
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("firebaseconfig.json");
+const router = express.Router();
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebaseconfig.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://qrdinger.firebaseio.com"
 });
 
-app.get('/', function(req, res){
+const db = admin.database();
 
-});
+router.route('/rapi/:uid/:code')
+  .get(function(req, res){
+    let userCodes = db.ref(`/codes/${req.params.uid}/`);
+    userCodes.on("value",(snapshot)=>{
+      let codes = snapshot.val();
+      for (var code in codes) {
+        let codeInstance = codes[code];
+        console.log(codeInstance)
+        if (codeInstance.model && codeInstance.model.type === 'redirect') {
+          console.log(codeInstance)
+          codeInstance.model.path === req.params.code;
+          res.redirect(codeInstance.model.redirect);
+        }
+      }
+    })
+  });
+
+app.use('', router);
 
 app.listen(3000);
